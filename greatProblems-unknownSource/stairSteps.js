bf58ea465ead52e7d9bdd29eb3752a20f48bb4d9
Yuyi <liyuyi828@gmail.com>
2016-10-21 00:26:28 -0700

    upload staitStep problem

---
+++ greatProblems-unknownSource/stairSteps.js
@@ -0,0 +1,18 @@
+// Source: unknown
+
+// A person can go up a stair with 1, 2 or 3 step at a time
+// Write a funtion to return how many possible ways a person can go up a stair with n stpes?
+
+// For example: stairStep(3); //return 4
+// because: possible combination (1, 1, 1), (2, 1), (1, 2), (3)
+
+// ** You can assume the input will always be a positive integer
+
+const stairSteps = (steps) => {
+  //You solution here:
+
+}
+
+// In case we need to write test in the future
+// module.exports = { stairStep }; 
+
